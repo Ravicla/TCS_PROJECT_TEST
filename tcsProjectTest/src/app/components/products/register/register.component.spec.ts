61345ca07ca391f9bc004576b17d1d11a1996ab8
Thalia <ravicla1993@gmail.com>
2023-09-24 20:23:24 -0500

    avance test unitarios

--- tcsProjectTest/src/app/components/products/register/register.component.spec.ts
+++ tcsProjectTest/src/app/components/products/register/register.component.spec.ts
@@ -1,21 +1,88 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
 
 import { RegisterComponent } from './register.component';
+import { ProductService } from 'src/app/services/products/product.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let productService: ProductService;
+  let activatedRoute: ActivatedRoute;
+  let router: Router;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [RegisterComponent]
-    });
+      declarations: [RegisterComponent],
+      imports: [ReactiveFormsModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ productId: '1' }) // Simula un parámetro en la ruta para la prueba
+          }
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate') // Simula la función navigate
+          }
+        },
+        {
+          provide: ProductService,
+          useValue: {
+            // Simula los métodos del servicio ProductService
+            validateId: (id: string) => Promise.resolve(true),
+            getAll: () => Promise.resolve([]),
+            createProduct: (product: any) => Promise.resolve({ id: '1' }),
+            updateProduct: (product: any) => Promise.resolve({ id: '1' })
+          }
+        }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    productService = TestBed.inject(ProductService);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    router = TestBed.inject(Router);
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize with "Enviar" as type', () => {
+    expect(component.type).toBe('Enviar');
+  });
+
+  it('should validate ID asynchronously', async () => {
+    const control = component.productForm.get('id');
+    if (control) { // Comprueba que control no sea nulo
+      control.setValue('123');
+      const result = await component.validateIdAsync(control);
+      expect(result).toBeNull(); // El ID debe ser válido en esta simulación
+    } else {
+      fail('Control is null'); // O maneja de otra manera si es null
+    }
+  });
+  
+
+  it('should navigate to /form-list after successful product creation', async () => {
+    const navigateSpy = spyOn(router, 'navigate'); // Espía la función navigate
+    spyOn(window, 'alert'); // Espía la función de alerta para evitar su ejecución real
+    const createProductSpy = spyOn(productService, 'createProduct').and.callThrough();
+    
+    component.getDataForm();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    
+    expect(createProductSpy).toHaveBeenCalledWith(component.productForm.value);
+    expect(navigateSpy).toHaveBeenCalledWith(['/form-list']);
+  });
+  
+
+  // Agrega más pruebas para otros casos según sea necesario
 });
--- tcsProjectTest/src/app/services/products/product.service.spec.ts
+++ tcsProjectTest/src/app/services/products/product.service.spec.ts
@@ -1,16 +1,65 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService],
+    });
+
     service = TestBed.inject(ProductService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make an HTTP GET request for getAll', () => {
+    const mockResponse = [
+      {
+        id: '1',
+        name: 'Product 1',
+      },
+      {
+        id: '2',
+        name: 'Product 2',
+      },
+    ];
+
+    service.getAll().then((data) => {
+      expect(data).toEqual(mockResponse);
+    });
+
+    const req = httpTestingController.expectOne('https://tribu-ti-staffing-desarrolloafangwbmcrhucqfh.z01.azurefd.net/ipf-msaproductosfinancieros/bp/products');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockResponse);
+  });
+
+  it('should make an HTTP GET request for validateId', () => {
+    const mockId = '123';
+
+    service.validateId(mockId).then((data) => {
+      expect(data).toEqual(true);
+    });
+
+    const req = httpTestingController.expectOne(`https://tribu-ti-staffing-desarrolloafangwbmcrhucqfh.z01.azurefd.net/ipf-msaproductosfinancieros/bp/products/verification?id=${mockId}`);
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(true);
+  });
+
+  // Agrega más pruebas para createProduct y updateProduct según sea necesario
 });
